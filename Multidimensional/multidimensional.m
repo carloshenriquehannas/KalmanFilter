50fb15fe6f760eea119c18ccf5faa427b08a91c2
carloshenriquehannas <carloshenriquehannas@gmail.com>
2024-03-07 11:16:48 -0300

    Atualizacao de comentarios

--- Multidimensional/multidimensional.m
+++ Multidimensional/multidimensional.m
@@ -3,7 +3,7 @@
 sigma_acel = 0.2*0.2;
 sigma_medicao = 3*3;
 
-x = [0 ; 0 ; 0 ; 0 ; 0 ; 0];
+x = zeros(6, 1);                                        %Inicializacao da matriz de estimativa
 
 Q = [0.25 0.5 0.5 0 0 0 ; 0.5 1 1 0 0 0 ; 0.5 1 1 0 0 0 ; 0 0 0 0.25 0.5 0.5 ; 0 0 0 0.5 1 1 ; 0 0 0 0.5 1 1]*(sigma_acel);
 
@@ -15,7 +15,7 @@ H = [1 0 0 0 0 0 ; 0 0 0 1 0 0 ];
 
 R = [sigma_medicao 0 ; 0 sigma_medicao];
 
-I = eye(6);
+I = eye(6);                                             %Matriz identidade 6x6
 
 Ft = transpose(F);
 Ht = transpose(H);
@@ -24,12 +24,13 @@ z = zeros(2, 1);                                        %Inicializacao da matriz
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%Laco de repeticao para medicao e estimacao de i fatores
 for i = 1:2
 
-    x = F*x;                                            %Predicao da posicao 
+    x = F*x;                                            %Predicao da estimativa 
     P = F*P*Ft + Q;                                     %Predicao da covariancia
 
-    %Entrada da matriz z pelo usuario
+    Entrada da matriz z pelo usuario
     for j = 1:2
         z(j) = input(sprintf('Digite o valor para a posição (%d, 1) da matriz: ', j));
     end                       
@@ -37,9 +38,9 @@ for i = 1:2
     K = P*Ht*(H*P*Ht + R)^(-1);                         %Predicao do ganho de Kalman
     Kt = transpose(K);                                  %Atualizacao da matriz transposta do ganho de Kalman                                  
 
-    x = x + K*(z - H*x);                                %Atualizacao da matriz de posicao
+    x = x + K*(z - H*x);                                %Atualizacao da matriz de estimativa
     P = (I - K*H)*P*(transpose(I - K*H)) + K*R*Kt;      %Atualizacao da matriz de covariancia
 
-    disp(x)
+    disp(x)                                             %Print da matriz de estimativa
 
 end
