024c4c40f99911f4a7b37354838cd040e487d354
carloshenriquehannas <carloshenriquehannas@gmail.com>
2024-04-12 12:01:47 -0300

    Modularizacao da matriz jacobiana

--- NaoLinear/extended01.m
+++ NaoLinear/extended01.m
@@ -1,19 +1,18 @@
 %Exemplo de sistema dinamico linear, mas com medicao nao-linear
-
 clear all
 close all
 
-%%%%%%%%%%%%%%%%%%%%%% DADOS INICIAIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DADOS INICIAIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-sigma_acel = 0.2;                  %Constante de desvio da aceleracao 
-sigma_medicao_posicao = 5;         %Desvio da medicao de posicao
-sigma_medicao_angulo = 0.0087;     %Desvio da medicao do angulo
-delta_tempo = 1;                   %Variacao do tempo
+sigma_acel = 0.2;                                                          %Constante de desvio da aceleracao 
+sigma_medicao_posicao = 5;                                                 %Desvio da medicao de posicao
+sigma_medicao_angulo = 0.0087;                                             %Desvio da medicao do angulo
+delta_tempo = 1;                                                           %Variacao do tempo
 
 %Matriz x 6x1 de estimativas
 x = zeros(6, 1); 
-x(1) = 400;                         %Posicao x inicial 
-x(4) = -300;                        %Posicao y inicial
+x(1) = 400;                                                                %Posicao x inicial 
+x(4) = -300;                                                               %Posicao y inicial
 
 %Matriz F e transposta Ft de transicao de estados. Linear neste exemplo
 F = [1, delta_tempo, (delta_tempo^2)/2, 0, 0, 0; 
@@ -27,7 +26,7 @@ Ft = transpose(F);
 %Matriz P 6x6 de covariancia de posicao
 P = zeros(6,6);          
 for i = 1:1:6           
-    P(i,i) = 500;       %Preenche a diagonal principal da matriz P
+    P(i,i) = 500;                                                          %Preenche a diagonal principal da matriz P
 end
 
 %Matriz Q de ruido da covariancia
@@ -42,28 +41,51 @@ Q = Q*(sigma_acel^2);
 %Matriz R de covariancia de ruido da medicao 
 R = [sigma_medicao_posicao^2 0 ; 0 sigma_medicao_angulo^2]; 
 
-%Define as constantes r e phi da matriz H. VERIFICAR O SINAL DE PHI
-r = x(1)^2 + x(4)^2;
-phi = atan(x(4)/x(1));
-H = [sqrt(r);
-     -phi];
+I = eye(6);                                                                %Matriz identidade
 
-%Define as matrizes jacobiana dH e sua transposta
-dH = [x(1)/sqrt(r) 0 0 x(4)/sqrt(r) 0 0 ;
-      -x(4)/r 0 0 x(1)/r 0 0];
-dHt = transpose(dH);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ALGORITMO DE KALMAN %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PRIMEIRA ITERACAO %%%%%%%%%%%%%%%%%%%%%%%%%%%
+for i = 1:1:2
+    x = F*x;                                                               %Predicao da estimativa
+    
+    P = F*P*Ft + Q;                                                        %Predicao da covariancia
+    
+    %Entrada da medicao do usuario
+    z(1,1) = input(sprintf('Digite o valor da medicao z1: '));
+    z(2,1) = input(sprintf('Digite o valor da medicao z2: '));
 
-x = F * x;                                  %Predicao da estimativa
+    %Funcao para calcular jacobiano de H
+    [r, phi, H, dH, dHt] = jacobiano(x);
+    
+    K = P*dHt*(dH*P*dHt + R)^(-1);                                         %Atualizacao do Ganho de Kalman
+    Kt = transpose(K);
+    
+    x = x + K*(z - H);                                                     %Atualizacao da estimativa
+    
+    P = (I - K*dH)*P*(transpose(I - K*dH)) + K*R*Kt;                       %Atualizacao da predicao
+end
 
-P = F * P * Ft + Q;                         %Predicao da covariancia
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-z = [502.55 ;
-     -0.93616];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CALCULO DO JACOBIANO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-K = P * dHt * (dH * P * dHt + R)^(-1);      %Atualizacao do Ganho de Kalman
+function [r, phi, H, dH, dHt] = jacobiano(x)
+    %Calcula r e phi
+    r = x(1)^2 + x(4)^2;
+    phi = atan(x(4)/x(1));
 
+    %Calcula H
+    H = [sqrt(r);
+         phi];
 
+    %Calcula dH: jacobiano de H
+    dH = [x(1)/sqrt(r) 0 0 x(4)/sqrt(r) 0 0 ;
+          -x(4)/r 0 0 x(1)/r 0 0];
+
+    %Matriz transposta do jacobiano de H
+    dHt = transpose(dH);
+end    
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
