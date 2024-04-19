32a8d7e0be0757fea75d1563c3d670948ca5be4e
carloshenriquehannas <carloshenriquehannas@gmail.com>
2024-04-19 11:32:22 -0300

    Correcao dos parametros

--- NaoLinear/medidas_extended01.m
+++ NaoLinear/medidas_extended01.m
@@ -3,8 +3,8 @@ close all
 
 %Gera trajetoria no plano
 
-posx(1) = 400;
-posy(1) = -300;
+posx(1) = 300;
+posy(1) = -400;
 
 velx(1) = 0;
 vely(1) = 0;
@@ -12,8 +12,8 @@ vely(1) = 0;
 acelx = 0.5;
 acely = 1;
 
-med_r(1) = 0;
-med_phi(1) = 0;
+%med_r(1) = 0;
+%med_phi(1) = 0;
 
 delta_tempo = 1;
 sigma_acel = 0.2;
@@ -27,8 +27,8 @@ for i = 1:1:99
     posx(i+1) = posx(i) + velx(i)*delta_tempo + (acelx + randn(1)*sigma_acel)*delta_tempo^2/2;
     posy(i+1) = posy(i) + vely(i)*delta_tempo + (acely + randn(1)*sigma_acel)*delta_tempo^2/2;
 
-    med_r(i+1) = sqrt(posx(i+1)^2 + posy(i+1)^2) + randn(1)*sigma_medicao_posicao;
-    med_phi(i+1) = atan(posy(i+1)/posx(i+1)) + + randn(1)*sigma_medicao_angulo;
+    med_r(i) = sqrt(posx(i+1)^2 + posy(i+1)^2) + randn(1)*sigma_medicao_posicao;
+    med_phi(i) = atan(posy(i+1)/posx(i+1)) + + randn(1)*sigma_medicao_angulo;
 
 end
 
