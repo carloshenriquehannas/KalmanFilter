adc3f5bd49cebceab1618a5aa97156507e7d908b
carloshenriquehannas <carloshenriquehannas@gmail.com>
2024-04-20 17:52:38 -0300

    Laco de iteracoes do filtro: sistema nao-linear

--- NaoLinear/extended02.m
+++ NaoLinear/extended02.m
@@ -7,7 +7,7 @@ close all
 sigma_acel_ang = 1;                                                        %Constante de desvio da aceleracao (rad/s^2)
 sigma_med_pos = 0.01;                                                      %Desvio da medicao de posicao (m)
 g = 9.8;                                                                   %Valor da gravidade g (m/s^2)
-delta_tempo = 0.05;                                                           %Variacao no tempo (s)
+delta_tempo = 0.05;                                                        %Variacao no tempo (s)
 L = 0.5;                                                                   %Valor da corda L (m)
 
 
@@ -51,30 +51,32 @@ I = eye(2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ALGORITMO DE KALMAN %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-F = [x(1)+x(2)*delta_tempo ; 
-     x(2) - (g/L)*sin(x(1))*delta_tempo];
-dF = [1 delta_tempo ; 
-     (-g/L)*cos(x(1))*delta_tempo 1];
-dFt = transpose(dF);
+for i=1:1:10
 
-x = F;                                                                     %Predicao da estimativa
+    F = [x(1)+x(2)*delta_tempo ; 
+         x(2) - (g/L)*sin(x(1))*delta_tempo];
+    dF = [1 delta_tempo ; 
+         (-g/L)*cos(x(1))*delta_tempo 1];
+    dFt = transpose(dF);
+    
+    x = F;                                                                 %Predicao da estimativa
+    
+    P = dF*P*dFt + Q;                                                      %Predicao da covariancia
+    
+    z = input(sprintf('Digite z: '));                                      %Dado de medicao da posicao                                     
+    
+    H = L*sin(x(1));
+    dH = [L*cos(x(1)) 0];
+    dHt = transpose(dH);
+    
+    K = P*dHt*(dH*P*dHt + R)^(-1);                                         %Atualizacao do Ganho de Kalman
+    Kt = transpose(K);
+    
+    x = x + K*(z - H);                                                     %Atualizacao da estimativa
+    
+    P = (I - K*dH)*P*(transpose(I - K*dH)) + K*R*Kt;                       %Atualizacao da covariancia
 
-P = dF*P*dFt + Q;                                                          %Predicao da covariancia
-
-z = 0.119;                                                                 %Dado de medicao da posicao                                     
-
-H = L*sin(x(1));
-dH = [L*cos(x(1)) 0];
-dHt = transpose(dH);
-
-K = P*dHt*(dH*P*dHt + R)^(-1);                                             %Atualizacao do Ganho de Kalman
-Kt = transpose(K);
-
-x = x + K*(z - H);                                                         %Atualizacao da estimativa
-
-P = (I - K*dH)*P*(transpose(I - K*dH)) + K*R*Kt;                           %Atualizacao da covariancia
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
 
