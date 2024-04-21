44407b8e6dd0c52898b8483f2b1082ad4542b8de
carloshenriquehannas <carloshenriquehannas@gmail.com>
2024-04-21 12:44:50 -0300

    Atualizacao arquivo de dado: sistema nao-linear

--- NaoLinear/medidas_extended02.m
+++ NaoLinear/medidas_extended02.m
@@ -4,11 +4,11 @@ clear all
 %Gera medidas do sistema
 
 sigma_med_pos = 0.01;
-theta =  0.0873;
 L = 0.5;
 
 for i=1:1:99
-    med_pos(i) = (L*sin(theta) + randn(1))*sigma_med_pos;
+    theta = rand()*2*pi;                                                   %Gera angulo theta entre 0 e 2*pi
+    med_ang(i) = (L*sin(theta) + randn(1))*sigma_med_pos;                  %Posicao em relacao ao angulo
 end
  
-save medidas_extended02.mat med_pos;
+save medidas_extended02.mat med_ang;
