b8cd405342c677549932fa4d58390f839c7d1ac0
carloshenriquehannas <carloshenriquehannas@gmail.com>
2024-05-09 15:50:24 -0300

    Jacobiano de H

--- NaoLinear/C/functions.h
+++ NaoLinear/C/functions.h
@@ -5,5 +5,5 @@
 void multiplica_matriz_quadrada(float matrizA[ROWS][COLUMNS], float matrizB[ROWS][COLUMNS], float resultado[ROWS][COLUMNS]);                                    //Multiplicacao de matrizes (matriz quadrada)
 void multiplica_matriz_coluna(float matrizA[ROWS][COLUMNS], float matrizB[ROWS][X_COLUMNS], float resultado[ROWS][X_COLUMNS]);                                  //Multiplicacao de matrizes (matriz coluna)
 void transpose_quadrada(float principal[ROWS][COLUMNS], float transposta[COLUMNS][ROWS]);                                                                       //Calcula matriz transposta (matriz quadrada)
-void transpose_H();
+void transpose_H(float principal[H_ROWS][COLUMNS], float transposta[COLUMNS][H_ROWS]);
 #endif
