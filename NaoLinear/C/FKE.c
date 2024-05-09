d3fdf7d452b91c075b890ab7c66b6d0bc8963bae
carloshenriquehannas <carloshenriquehannas@gmail.com>
2024-05-09 11:40:50 -0300

    Correcao da predicao

--- NaoLinear/C/FKE.c
+++ NaoLinear/C/FKE.c
@@ -3,33 +3,53 @@
 #include "initialize.h"
 #include "FKE.h"
 
-float multiplica_matriz_coluna(float matrizA[ROWS][X_COLUMNS], float matrizB[ROWS][COLUMNS]){
-    int i, j, k;
-    float resultado[COLUMNS][ROWS];
+//Funcao para fazer a multiplicacao de matrizes, tendo matrizes quadradas
+void multiplica_matriz_quadrada(float matrizA[ROWS][COLUMNS], float matrizB[ROWS][COLUMNS], float resultado[ROWS][COLUMNS]){
+    int i,j,k;
 
-    //Inicializa a matriz resultado com zeros
-    for (i = 0; i < ROWS; i++){                                                                                         //Percorre linhas da matriz
-        for (j = 0; j < X_COLUMNS; j++){                                                                                //Percoore colunas da matriz
-            resultado[i][j] = 0;                                                                                        //Preenche a matriz com zeros
-        }
-    }
-
-    //Calcula a multiplicação das matrizes
-    for(i = 0; i < ROWS; i++){                                                                                          //Percorre linhas das matrizes
-        for(j = 0; j < COLUMNS; j++){                                                                                   //Percorre colunas da matriz B
-            for(k = 0; k < X_COLUMNS; k++){                                                                             //Percorre colunas da matriz A
-                resultado[i][j] += matrizA[i][k] * matrizB[k][j];                                                       //Armazena o resultado da multiplicacao
+    //Calcula a multiplicacao das matrizes
+    for(i = 0; i < ROWS; i++){
+        for(j = 0; j < COLUMNS; j++){
+            resultado[i][j] = 0;                                                                                        //Inicializa elemento da matriz resultado com zero
+            for(k = 0; k < COLUMNS; k++){                                                                               //Percorre colunas da matriz A
+                resultado[i][j] += matrizA[i][k] * matrizB[k][j];                                                       //Armazena o elemento da multiplicacao na matriz resultado
             }
         }
     }
+}
 
-    return resultado[ROWS][X_COLUMNS];
+//Funcao para fazer a multiplicacao de matrizes, tendo uma matriz coluna
+void multiplica_matriz_coluna(float matrizA[ROWS][COLUMNS], float matrizB[ROWS][X_COLUMNS], float resultado[ROWS][X_COLUMNS]){
+    int i, j, k;
+
+    //Calcula a multiplicacao das matrizes
+    for(i = 0; i < ROWS; i++){                                                                                          //Percorre linhas das matrizes
+        for(j = 0; j < X_COLUMNS; j++){                                                                                 //Percorre colunas da matriz B
+            resultado[i][j] = 0;                                                                                        //Inicializa elemento da matriz resultado com zero
+            for(k = 0; k < COLUMNS; k++){                                                                               //Percorre colunas da matriz A
+                resultado[i][j] += matrizA[i][k] * matrizB[k][j];                                                       //Armazena o elemento da multiplicacao na matriz resultado
+            }
+        }
+    }
 }
 
 void extended_kalman(float x[ROWS][X_COLUMNS], float F[ROWS][COLUMNS], float P[ROWS][COLUMNS], float Ft[COLUMNS][ROWS], float Q[ROWS][COLUMNS]){
+    float P_aux[ROWS][COLUMNS];                                                                                         //Matriz auxiliar para predicao
+    float x_res[ROWS][X_COLUMNS];                                                                                       //Matriz de resultado para estimativa
 
-    x[ROWS][X_COLUMNS] = multiplica_matriz_coluna(x, F);
+    //Predicao da estimativa: x_res = F * x
+    multiplica_matriz_coluna(F, x, x_res);
 
-    //CONTINUAR A PARTIR DA PREDICAO DA COVARIANCIA
+    //Predicao da covariancia: P = F * P * Ft + Q
+    multiplica_matriz_quadrada(P, Ft, P_aux);                                                                           //P_aux = P * Ft
+    multiplica_matriz_quadrada(F, P_aux, P);                                                                            //P = F * P_aux
+    P[ROWS][COLUMNS] = P[ROWS][COLUMNS] + Q[ROWS][COLUMNS];
+
+    /*for(int i = 0; i < ROWS; i++){
+        for(int j = 0; j < COLUMNS; j++){
+            printf("%f\t", P[i][j]);
+        }
+        printf("\n");
+    }*/
 
 }
--- NaoLinear/C/FKE.h
+++ NaoLinear/C/FKE.h
@@ -2,7 +2,8 @@
 #ifndef MAIN_C_FKE_H
 #define MAIN_C_FKE_H
 
-float multiplica_matriz_coluna(float matrizA[ROWS][X_COLUMNS], float matrizB[ROWS][COLUMNS]);                           //Multiplicacao de matrizes (matriz coluna)
-void extended_kalman(float x[ROWS][X_COLUMNS], float F[ROWS][COLUMNS], float P[ROWS][COLUMNS], float Ft[COLUMNS][ROWS], float Q[ROWS][COLUMNS]);      //Algoritmo de Kalman
+void multiplica_matriz_quadrada(float matrizA[ROWS][COLUMNS], float matrizB[ROWS][COLUMNS], float resultado[ROWS][COLUMNS]);                              //Multiplicacao de matrizes (matriz quadrada)
+void multiplica_matriz_coluna(float matrizA[ROWS][COLUMNS], float matrizB[ROWS][X_COLUMNS], float resultado[ROWS][X_COLUMNS]);                                  //Multiplicacao de matrizes (matriz coluna)
+void extended_kalman(float x[ROWS][X_COLUMNS], float F[ROWS][COLUMNS], float P[ROWS][COLUMNS], float Ft[COLUMNS][ROWS], float Q[ROWS][COLUMNS]);          //Algoritmo de Kalman
 
 #endif
